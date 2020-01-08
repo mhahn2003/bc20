d4ffdc417b7e520e02f1585b0e1d28c5ad22b8e5
xmy314 <44828636+xmy314@users.noreply.github.com>
2020-01-07 19:01:20 -0800

    start working on broad casting

---
+++ src/bot01/Cast.java
@@ -0,0 +1,32 @@
+package bot01;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+
+import java.util.LinkedList;
+
+// Navigation class
+public class Cast {
+
+    public static int ukey;
+
+    public enum information_catagory{
+        // new building(needs coordinate and type)
+        new_building,
+        // destroyed building(needs coordinate)
+        remove_building,
+        // found a soup repository
+        new_soup,
+    }
+
+    
+
+    public Cast() {
+
+    }
+
+
+
+}
