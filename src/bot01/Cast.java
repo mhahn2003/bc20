570ebd273e2cea03d15ce42d88786515b40758d7
xmy314 <44828636+xmy314@users.noreply.github.com>
2020-01-07 20:55:53 -0800

    finish basic broadcasting information procesing

--- src/bot01/Cast.java
+++ src/bot01/Cast.java
@@ -19,20 +19,13 @@ public class Cast {
     
 
     public static int broadCast(information_catagory cat ) {
-        int message=0;
         switch (cat) {
-            case NEW_BUILDING:
-                message = 1;
-            break;
-            case REMOVE_BUILDING:
-                message = 2;
-            break;
-            case NEW_SOUP:
-                message = 3;
-            break;
+            case NEW_BUILDING:      return 1;
+            case REMOVE_BUILDING:   return 2;
+            case NEW_SOUP:          return 3;
+            case OTHER:             return 0;
+            default:                return 0;
         }
-
-        return message;
     }
 
     public static int addCoord(int info_ori,int[] pos){
@@ -41,17 +34,11 @@ public class Cast {
 
     public static information_catagory get_cat(int message){
         switch((int)Math.floor(message/10000)){
-            case 1:  
-            return information_catagory.NEW_BUILDING;
-            case 2:  
-            return information_catagory.REMOVE_BUILDING;
-            case 3:  
-            return information_catagory.NEW_SOUP;
-            default:
-            return information_catagory.OTHER;
+            case 1:  return information_catagory.NEW_BUILDING;
+            case 2:  return information_catagory.REMOVE_BUILDING;
+            case 3:  return information_catagory.NEW_SOUP;
+            default: return information_catagory.OTHER;
         }
-        // need to return
-        return null;
     }
 
     public static int[] getCoord(int message){
