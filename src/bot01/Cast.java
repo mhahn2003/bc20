b26bedba4064096ea73524ce558c1a9b197ca3e3
xmy314 <44828636+xmy314@users.noreply.github.com>
2020-01-07 20:50:27 -0800

    finish basic broadcasting information processing

--- src/bot01/Cast.java
+++ src/bot01/Cast.java
@@ -19,7 +19,9 @@ public class Cast {
         // destroyed building(needs coordinate)
         REMOVE_BUILDING,
         // found a soup repository
-        NEW_SOUP
+        NEW_SOUP,
+        // enemy?
+        OTHER
     }
 
     
@@ -47,11 +49,20 @@ public class Cast {
 
     public static information_catagory get_cat(int message){
         switch((int)Math.floor(message/10000)){
-            case 1: return information_catagory.NEW_BUILDING;
-            case 2: return information_catagory.REMOVE_BUILDING;
-            case 3: return information_catagory.NEW_SOUP;
+            case 1:  
+            return information_catagory.NEW_BUILDING;
+            case 2:  
+            return information_catagory.REMOVE_BUILDING;
+            case 3:  
+            return information_catagory.NEW_SOUP;
+            default:
+            return information_catagory.OTHER;
         }
     }
 
+    public static int[] getCoord(int message){
+        int[] coord={message%10000-message%100, message%100};
+        return coord;
+    }
 
 }
