e54d821bb43ba7d9245f04c4019d3060992b9f10
xmy314 <44828636+xmy314@users.noreply.github.com>
2020-01-10 20:22:38 -0800

    pseudo code for attacking

---
+++ src/bot01/Attack.java
@@ -0,0 +1,15 @@
+package bot01;
+
+import battlecode.common.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Attack {
+
+
+
+    
+}
